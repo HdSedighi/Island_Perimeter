59fa1a2690046f273ea9729444df5d4b544d2fce
Hoda Sedighi <sedighi.iut@gmail.com>
2024-04-18 10:37:39 -0400

    call and thest the solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program{
+ private static void Main(string[] args){
+     int[][] grid = new int[][]{
+
+        //[[0,1]]
+        new int[]{1,1},
+        //  new int[]{0,1,0,0},
+        //  new int[]{1,1,1,0},
+        //  new int[]{0,1,0,0},
+        //  new int[]{1,1,0,0}
+     };
+     Solution sol = new Solution();
+     Console.WriteLine(sol.IslandPerimeter(grid));
+ }
+
+}
+
+
